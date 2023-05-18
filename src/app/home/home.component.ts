53734209e1eea0b71d8e9b32d841d4c12838a2bb
gryxis <felix@mspcblayout.de>
2023-05-18 16:03:26 +0200

    introduce the portfolio website
    with a small dashboard for the upcoming projects.

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+type Project = {
+  title: string;
+  description: string;
+  link: string;
+};
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  projects: Project[] =  [
+    {
+      title: 'Game of Life',
+      link: 'game-of-live',
+      description: 'a small implementation of the game of live.',
+    },
+  ];
+}
